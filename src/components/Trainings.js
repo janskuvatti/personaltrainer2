ce3e885ed9f924c1a8e7f68a024ecaee2ce73353
janskuvatti <janina.porkka@gmail.com>
2019-12-18 14:15:12 +0200

    fixed deleteTraining function

--- src/components/Trainings.js
+++ src/components/Trainings.js
@@ -1,3 +1,4 @@
+
 import React, {useState, useEffect} from 'react';
 import Button from '@material-ui/core/Button';
 
@@ -7,32 +8,34 @@ import moment from 'moment';
 
 const ListAllTrainings = () =>{
     const [trainings, setTrainigs] = useState([]);
-
     useEffect(() => fetchTrainings(), []);
-    const deleteTraining = ( link) => {
-        console.log(link);
-        if(window.confirm("Are you sure you want to delete this training?")){
+    const deleteTraining = (link) => {
+      console.log(link);
+          if(window.confirm("Are you sure you want to delete this training?")){
         fetch(link, {method:'DELETE'})
         .then(res => fetchTrainings())
-        
-       
+
         .catch(err => console.error(err))
 
         }
+
     }
+   
     const fetchTrainings = () => {
-        fetch('https://customerrest.herokuapp.com/gettrainings')
+        fetch('https://customerrest.herokuapp.com/api/trainings')
         .then(response => response.json())
-        .then(data => setTrainigs(data))
+        .then(data => setTrainigs(data.content))
     
     }
        
-    
+
+       
     const columns = [
           {
             Header: "Date",
             accessor: "date",
-         Cell: row => moment(row.value).format('DD.MM.YYYY  HH:MM')
+         Cell: row => moment(row.value).format('DD.MM.YYYY  HH:mm')
+         
      
     },
 
@@ -49,22 +52,25 @@ const ListAllTrainings = () =>{
         },
         {
             Header: "customer_Firstname",
-            accessor: "customer.firstname"
-            },
+      accessor: "customer.lastname"
+
+        },
             {
-                Header: "customr_Lastname",
-                accessor: "customer.lastname"
+                Header: "customer_Lastname",
+                accessor: "lastname"
                 },
-        {
-            sortable: false,
-            filterable: false,
-            width: 100,
-           accessor: "links[0].href",
-          Cell: row => <Button variant="contained" color="secondary" size="small" onClick={() => deleteTraining(row.value)}>Delete</Button>
-                                  
-                               
-       }
-    
+      
+                {
+                    sortable: false,
+                    filterable: false,
+                    width: 100,
+                   accessor: "links[0].href",
+                  Cell: row => <Button variant="contained" color="secondary" size="small" onClick={() => deleteTraining(row.value)}>Delete</Button>
+                                          
+                                       
+               },
+              
+
     ]
 return(
     <div>
